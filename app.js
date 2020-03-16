f578484b39446d0ad718f0f9ff3d581d7566034f
Léa De Amorin <leadeamorin.ps@gmail.com>
2020-03-16 13:28:48 +0100

    06

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
 app.get('/', function(req, res) {
-    res.send('Coucou');
+    res.render('index');
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
