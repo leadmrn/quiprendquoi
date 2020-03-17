9420a40f356de5a39b46fa9c218f10bb172c5c9a
Léa De Amorin <leadeamorin.ps@gmail.com>
2020-03-17 12:16:31 +0100

    10

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ app.set('view engine', 'pug');
 app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
 });
+app.post('/party', function(req, res) {
+    res.send('Post ok !');
+});
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
