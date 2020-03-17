0ac5bbc9fdccbe7220e8e867a58132c5bd4437fe
Léa De Amorin <leadeamorin.ps@gmail.com>
2020-03-17 12:45:26 +0100

    12

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
@@ -9,8 +10,11 @@ app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
 });
 app.post('/party', function(req, res) {
-    res.send('Post ok !');
+    axios
+        .post(`${process.env.API_URL}/party`, req.body)
+        .then(({data}) => console.log(data))
+        .catch((err) => console.error(err));
     console.log(req.body);
 });
 
-app.listen(port, () => console.log(`Front app listening on port ${port}!`));
+app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
