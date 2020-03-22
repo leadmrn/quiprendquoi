0a4b4d0aeb5d9cac298eddf49246e71912992f0b
Léa De Amorin <leadeamorin.ps@gmail.com>
2020-03-22 22:00:29 +0100

    33

--- sw.js
+++ sw.js
@@ -4,3 +4,8 @@ console.log(`Hello ${name}`);
 addEventListener('install', (event) => {
     console.log('Hello from the service worker')
 });
+
+addEventListener('fetch', (event) => {
+    console.log(event);
+    event.respondWith(fetch(event.request));
+});
